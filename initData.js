c308fad0e8d444875e9981d36f6cb8dacdfe3dd4
HoHoangLien <hoho0001@algonquinlive.com>
2019-09-29 23:28:08 -0400

    Finish CU

---
+++ initData.js
@@ -0,0 +1,155 @@
+var db = firebase.firestore();
+var categoriesRef = db.collection("GameOfThrones");
+
+categoriesRef.doc("Stark").set({
+    name: "Stark", 
+    parentId: ""
+})
+categoriesRef.doc("Tully").set({
+  name: "Tully", 
+  parentId: ""
+})
+categoriesRef.doc("Targaryen").set({
+  name: "Targaryen", 
+  parentId: ""
+})
+categoriesRef.doc("Martell").set({
+  name: "Martell", 
+  parentId: ""
+})
+categoriesRef.doc("Lannister").set({
+  name: "Lannister", 
+  parentId: ""
+})
+categoriesRef.doc("Greyjoy").set({
+  name: "Greyjoy", 
+  parentId: ""
+})
+categoriesRef.doc("Baratheon").set({
+  name: "Baratheon", 
+  parentId: ""
+})
+categoriesRef.doc("Arryn").set({
+  name: "Arryn", 
+  parentId: ""
+})
+categoriesRef.doc("EddardStark").set({
+  name: "Eddard Stark", 
+  parentId: "Stack"
+})
+categoriesRef.doc("RobertBaratheon").set({
+  name: "Robert Baratheon", 
+  parentId: "Baratheon"
+})
+categoriesRef.doc("JaimeLannister").set({
+  name: "Jaime Lannister", 
+  parentId: "Lannister"
+})
+categoriesRef.doc("CatelynStark").set({
+  name: "Catelyn Stark", 
+  parentId: "Stark"
+})
+categoriesRef.doc("	CerseiLannister").set({
+  name: "	Cersei Lannister", 
+  parentId: "Lannister"
+})
+categoriesRef.doc("DaenerysTargaryen").set({
+  name: "Daenerys Targaryen", 
+  parentId: "Targaryen"
+})
+categoriesRef.doc("ViserysTargaryen").set({
+  name: "Viserys Targaryen", 
+  parentId: "Targaryen"
+})
+categoriesRef.doc("JonSnow").set({
+  name: "Jon Snow", 
+  parentId: "Stark"
+})
+categoriesRef.doc("SansaStark").set({
+  name: "Sansa Stark", 
+  parentId: "Stark"
+})
+categoriesRef.doc("AryaStark").set({
+  name: "Arya Stark", 
+  parentId: "Stark"
+})
+categoriesRef.doc("TheonGreyjoy").set({
+  name: "Theon Greyjoy", 
+  parentId: "Greyjoy"
+})
+categoriesRef.doc("JoffreyBaratheon").set({
+  name: "Joffrey Baratheon", 
+  parentId: "Baratheon"
+})
+categoriesRef.doc("TyrionLannister").set({
+  name: "Tyrion Lannister", 
+  parentId: "Lannister"
+})
+categoriesRef.doc("StannisBaratheon").set({
+  name: "Stannis Baratheon", 
+  parentId: "Baratheon"
+})
+categoriesRef.doc("EdmureTully").set({
+  name: "Edmure Tully", 
+  parentId: "Tully"
+})
+categoriesRef.doc("BryndenTully").set({
+  name: "Brynden Tully", 
+  parentId: "Tully"
+})
+categoriesRef.doc("OberynMartell").set({
+  name: "Oberyn Martell", 
+  parentId: "Martell"
+})
+categoriesRef.doc("YaraGreyjoy").set({
+  name: "Yara Greyjoy", 
+  parentId: "Greyjoy"
+})
+categoriesRef.doc("DoranMartell").set({
+  name: "Doran Martell", 
+  parentId: "Martell"
+})
+categoriesRef.doc("LysaArryn").set({
+  name: "Lysa Arryn", 
+  parentId: "Arryn"
+})
+categoriesRef.doc("RobinArryn").set({
+  name: "Robin Arryn", 
+  parentId: "Arryn"
+})
+categoriesRef.doc("JonArryn").set({
+  name: "Jon Arryn", 
+  parentId: "Arryn"
+})
+categoriesRef.doc("TywinLannister").set({
+  name: "Tywin Lannister", 
+  parentId: "Lannister"
+})
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
+// categoriesRef.doc("").set({
+//   name: "", 
+//   parentId: ""
+// })
